fd2ffecd421f4c1cbddeec932bdb02a6995a75ba
Light Yagami <freelance0825@gmail.com>
2025-03-21 15:30:47 +0800

    Initial Commit

---
+++ src/main/java/com/freelance/thunderscope_scalable_backend/ThunderscopeScalableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.thunderscope_scalable_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderscopeScalableBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderscopeScalableBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelance/thunderscope_scalable_backend/ThunderscopeScalableBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelance.thunderscope_scalable_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderscopeScalableBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
